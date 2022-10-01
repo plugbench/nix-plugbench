93f246a5f0d13630b899680dafa636b355a5ad2f
Jason Felice <jason.m.felice@gmail.com>
2022-10-01 17:26:14 -0400

    Bump plumber-pluggo

--- pkg/plumber-pluggo.nix
+++ pkg/plumber-pluggo.nix
@@ -1,6 +1,6 @@
 {
   owner = "plugbench";
   repo = "plumber-pluggo";
-  rev = "4f70c26378ad3b2fabb647a2904df9eefd13106c";
-  sha256 = "eHsjEH087rK545h4xACmMLQ2m8mXX8W7WnRjRhRjUDM=";
+  rev = "4d3e8e9adc48731bec5c71c716fb6921ae03addc";
+  sha256 = "k7PCuxf0jp1XeIXLYO4h78jcTLUUm3hi2tzu0KI5Z6c=";
 }
