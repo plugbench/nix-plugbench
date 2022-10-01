5c5df7c3381941574ec852c72faa23646b822a35
Jason Felice <jason.m.felice@gmail.com>
2022-10-01 14:20:30 -0400

    Fix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "TODO: fill me in";
+  description = "Derivations and modules for installing plugbench with Nix.";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -7,21 +7,17 @@
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        nix-plugbench = pkgs.callPackage ./derivation.nix {};
       in {
         packages = {
-          default = nix-plugbench;
-          inherit nix-plugbench;
         };
         checks = {
           test = pkgs.runCommandNoCC "nix-plugbench-test" {} ''
             mkdir -p $out
-            : ${nix-plugbench}
+            :
           '';
         };
     })) // {
       overlays.default = final: prev: {
-        nix-plugbench = prev.callPackage ./derivation.nix {};
       };
     };
 }
