3793e5057e94f42f4a05d6bb14c515b300f5d305
Jason Felice <jason.m.felice@gmail.com>
2022-10-02 13:21:47 -0400

    Bump kakoune

--- pkg/kakoune-pluggo.nix
+++ pkg/kakoune-pluggo.nix
@@ -1,6 +1,6 @@
 {
   owner = "plugbench";
   repo = "kakoune-pluggo";
-  rev = "9dcb8241cb33b55ebb7a89f181bd3a46c122bc83";
-  sha256 = "8Yq7Gs1WOOSYtQTwWcMGvcm0Cnds+y5DqSKNOH5akhc=";
+  rev = "f38d78b3910719038f02e0cbbd1300bf54af190e";
+  sha256 = "HMItk5jY0HPtXQ3CY86d7MIp3+bp1yPPIH1ioUiNI8Q=";
 }
