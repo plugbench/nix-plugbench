b76eb49a3ca76756db7aeb2f27a56984bcc8d442
Jason Felice <jason.m.felice@gmail.com>
2022-10-12 09:43:06 -0400

    Automatically initialize kakoune

--- nix-darwin/modules/kakoune/default.nix
+++ nix-darwin/modules/kakoune/default.nix
@@ -3,11 +3,36 @@
 with lib;
 let
   cfg = config.plugbench.kakoune;
+
 in {
   options.plugbench.kakoune.enable = mkEnableOption "plugbench kakoune";
 
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.kakoune-pluggo ];
+
+    nixpkgs.overlays = [
+      (final: prev: let
+         pluggo-init = prev.mkDerivation {
+           name = "kakoune-pluggo-init";
+
+           installPhase = ''
+             runHook preInstall
+
+             target="$out/share/kak/autoload/plugins"
+             mkdir -p "$target"
+             cat <<EOF >"$target/kakoune-pluggo-init.kak"
+             evaluate-commands %sh{${final.kakoune-pluggo}/bin/kakoune-pluggo-start-session "$kak_session"}
+             EOF
+
+             runHook postInstall
+           '';
+         };
+       in {
+         kakoune = prev.kakoune.overrideAttrs (finalAttrs: prevAttrs: {
+           paths = prevAttrs.paths ++ [ pluggo-init ];
+         })
+       })
+    ];
 
     launchd.user.agents.plugbench-kakoune = {
       path = [ pkgs.kakoune ];
