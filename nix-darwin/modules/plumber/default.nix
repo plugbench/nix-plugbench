513fd7bbae15f74e7177e5d55a7f35ced3a696b7
Jason Felice <jason.m.felice@gmail.com>
2024-02-13 11:26:51 -0500

    Bump

--- nix-darwin/modules/plumber/default.nix
+++ nix-darwin/modules/plumber/default.nix
@@ -2,6 +2,7 @@
 
 with lib;
 let
+  token = config.plugbench.token;
   cfg = config.plugbench.plumber;
 in {
   config = mkIf cfg.enable {
@@ -9,10 +10,10 @@ in {
       script = ''
         ${pkgs.plumber-pluggo}/bin/plumber
       '';
+      environment.NATS_TOKEN = mkIf (token != null) token;
       serviceConfig = {
         KeepAlive = true;
       };
     };
   };
-
 }
